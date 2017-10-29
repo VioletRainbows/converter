ec2a05022d9e614df5eb5eb5385412110333c4d2
Mia Boulay <mia.boulay@linux.com>
2017-10-29 14:41:19 -0400

    Add STDIN and STDOUT as option

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+// Usage: convert [SOURCE] [DEST]
+// Convert ISO-8859-1 SOURCE to UTF-8 DEST
+//
+// SOURCE: STDIN or FILE
+// DEST: STDOUT or FILE
+//
+// Examples:
+// convert < latin1.txt > utf8.xml
+// convert latin1.txt utf8.xml
+// convert latin1.txt                # Output to STDOUT
+// convert                           # CTRL+D to exit
+
 extern crate encoding;
+
+use std::io::{self, Read};
 
 use encoding::{DecoderTrap, Encoding};
 use encoding::all::ISO_8859_1;
@@ -6,39 +20,51 @@ use encoding::all::ISO_8859_1;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-use std::path::Path;
-use std::process;
+use std::error::Error;
 
-const BUFSIZE: usize = 4096;
+const BUFSIZE: usize = 8192;
+
+struct Config<R: io::Read, W: io::Write> {
+    source: R,
+    dest: W,
+}
+
+impl Config<Box<io::Read>, Box<io::Write>> {
+    fn new(args: &[String]) -> Result<Self, Box<Error>> {
+        if args.len() == 1 {
+            Ok(Config {
+                source: Box::new(io::stdin()),
+                dest: Box::new(io::stdout()),
+            })
+        } else if args.len() == 2 {
+            Ok(Config {
+                source: Box::new(File::open(args[1].clone())?),
+                dest: Box::new(io::stdout()),
+            })
+        } else {
+            Ok(Config {
+                source: Box::new(File::open(args[1].clone())?),
+                dest: Box::new(File::create(args[2].clone())?),
+            })
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    if args.len() != 3 {
-        eprintln!("Usage: {} SOURCE DEST", args[0]);
-        eprintln!("Convert ISO-8859-1 SOURCE to UTF-8 DEST");
-        process::exit(1);
-    }
-
-    // File for reading
-    let pread = Path::new(&args[1]);
-    let mut fread = File::open(&pread).unwrap();
-
-    // File for writing
-    let pwrite = Path::new(&args[2]);
-    let mut fwrite = File::create(&pwrite).unwrap();
+    let mut config = Config::new(&args).unwrap();
 
     // Reading and converting BUFSIZE bytes at a time
     let mut bytes = vec![0u8; BUFSIZE];
     while {
-        let count = fread.read(bytes.as_mut_slice()).unwrap();
-        fwrite.write(
-            // Convert from ISO-8859-1 (latin-1) to UTF-8
-            ISO_8859_1
+        let count = config.source.read(bytes.as_mut_slice()).unwrap();
+        config.dest.write(
+             // Convert from ISO-8859-1 (latin-1) to UTF-8
+             ISO_8859_1
                 .decode(&mut bytes[0..count], DecoderTrap::Strict)
                 .unwrap()
                 .as_bytes(),
         ).unwrap();
-        count == BUFSIZE
+        count != 0
     } {}
 }
