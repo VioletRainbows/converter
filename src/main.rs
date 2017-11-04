b48a814f3944f4c5c37064a85827a36d03aa4033
Mia Boulay <mia.boulay@linux.com>
2017-11-04 13:48:10 -0400

    Use BufReader and BufWriter

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,32 @@ use encoding::{DecoderTrap, Encoding};
 use encoding::all::ISO_8859_1;
 
 use std::env;
-use std::fs::File;
 use std::error::Error;
-use std::io;
+use std::fs::File;
+use std::io::{Read, Write, stdin, stdout};
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::io::BufWriter;
 use std::process;
 
 const BUFSIZE: usize = 8192;
 
-struct Config<R: io::Read, W: io::Write> {
+struct Config<R: Read, W: Write> {
     source: R,
     dest: W,
 }
 
-impl Config<Box<io::Read>, Box<io::Write>> {
+impl Config<Box<Read>, Box<Write>> {
     fn new(args: &[String]) -> Result<Self, Box<Error>> {
         if args.len() == 1 {
             Ok(Config {
-                source: Box::new(io::stdin()),
-                dest: Box::new(io::stdout()),
+                source: Box::new(stdin()),
+                dest: Box::new(stdout()),
             })
         } else if args.len() == 2 {
             Ok(Config {
                 source: Box::new(File::open(args[1].clone())?),
-                dest: Box::new(io::stdout()),
+                dest: Box::new(stdout()),
             })
         } else {
             Ok(Config {
@@ -44,12 +47,17 @@ fn iso_8859_1_to_utf_8(input: &[u8]) -> Result<String, String> {
     Ok(ISO_8859_1.decode(input, DecoderTrap::Strict)?)
 }
 
-fn decode<R: io::Read, W: io::Write>(source: &mut R, dest: &mut W) -> Result<(), Box<Error>>{
+fn decode<R: Read, W: Write>(source: &mut R, dest: &mut W) -> Result<(), Box<Error>> {
+    let mut reader = BufReader::new(source);
+    let mut writer = BufWriter::new(dest);
+
     // Reading and converting BUFSIZE bytes at a time
     let mut bytes = vec![0u8; BUFSIZE];
     while {
-        let count = source.read(bytes.as_mut_slice())?;
-        dest.write(iso_8859_1_to_utf_8(&bytes[0..count])?.as_bytes())?;
+        let count = reader.read(bytes.as_mut_slice())?;
+        writer.write(
+            iso_8859_1_to_utf_8(&bytes[0..count])?.as_bytes(),
+        )?;
         count != 0
     } {}
     Ok(())
@@ -78,7 +86,7 @@ fn main() {
         Err(error) => {
             eprintln!("There was a file problem: {}", error);
             process::exit(1);
-        },
+        }
     };
 
     // I tried to pass binaries and no error came, so I do not know
@@ -90,7 +98,7 @@ fn main() {
         Err(error) => {
             eprintln!("There was an encoding problem: {}", error);
             process::exit(1);
-        },
+        }
         _ => (),
     };
 }
@@ -98,11 +106,14 @@ fn main() {
 #[cfg(test)]
 mod test {
     use super::*;
-    
+
     #[test]
     fn it_decode_iso_8859_1_to_utf_8() {
         // Letter 'é' (different in ISO-8859-1 and UTF-8)
-        assert_eq!(iso_8859_1_to_utf_8(&[0xe9]).unwrap().as_bytes(), [0xc3, 0xa9]);
+        assert_eq!(
+            iso_8859_1_to_utf_8(&[0xe9]).unwrap().as_bytes(),
+            [0xc3, 0xa9]
+        );
 
         // Letter 'a'
         assert_eq!(iso_8859_1_to_utf_8(&[0x61]).unwrap(), "a");
@@ -114,13 +125,13 @@ mod test {
     #[test]
     fn it_decode() {
         use std::io::Cursor;
-        
+
         // Gives "ABCabc êý¿÷" in ISO-8859-1
         let mut source = Cursor::new(
-            &[0x41, 0x42, 0x43, 0x61, 0x62, 0x63, 0x20, 0xea, 0xfd, 0xbf, 0xf7]
+            &[0x41, 0x42, 0x43, 0x61, 0x62, 0x63, 0x20, 0xea, 0xfd, 0xbf, 0xf7],
         );
         let mut dest = Cursor::new(vec![0u8]);
-        
+
         decode(&mut source, &mut dest).unwrap();
 
         // Works as string is already in UTF-8
